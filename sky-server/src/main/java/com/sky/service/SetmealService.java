b726bb6f1392f3d9a3cc10c0dc00a0fefd18ea18
Kun CHEN <scykc5@xmail.nottingham.ac.uk>
2026-03-26 22:15:18 +0000

    小程序端展示

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
 import com.sky.result.PageResult;
+import com.sky.vo.DishItemVO;
 
 import java.util.List;
 
@@ -15,4 +17,8 @@ public interface SetmealService {
     void deleteBatch(List<Long> ids);
 
     void update(SetmealDTO setmealDTO);
+
+    List<DishItemVO> getDishItemById(Long id);
+
+    List<Setmeal> list(Setmeal setmeal);
 }
