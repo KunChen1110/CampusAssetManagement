6b489d5477b2c52244541e1613a6a1a855cc3684
Kun CHEN <scykc5@xmail.nottingham.ac.uk>
2026-03-31 09:49:32 +0100

    订单数据可视化显示

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -1,8 +1,12 @@
 package com.sky.controller.admin;
 
+import com.sky.entity.OrderDetail;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,4 +29,29 @@ public class ReportController {
 
         return Result.success(reportService.getTurnoverStatistics(begin,end));
     }
+
+    @GetMapping("/userStatistics")
+    public Result<UserReportVO> userStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+
+        return Result.success(reportService.getUserStatistics(begin,end));
+    }
+
+    @GetMapping("/ordersStatistics")
+    public Result<OrderReportVO> ordersStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+
+        return Result.success(reportService.getOrderStatistics(begin,end));
+    }
+
+    @GetMapping("/top10")
+    public Result<SalesTop10ReportVO> top10(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+
+        return Result.success(reportService.getSalesTop10(begin,end));
+    }
+
 }
